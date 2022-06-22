09420f046a64e1f1634ec972003b4b0a0f16808f
unknown <superstargajanikant@gmail.com>
2022-06-22 11:18:16 +0545

    changes views

--- food_app/urls.py
+++ food_app/urls.py
@@ -3,10 +3,6 @@ from django.urls import path
 
 from . import views as food_views
 urlpatterns = [
-<<<<<<< HEAD
-    path('', ),
-=======
-   
->>>>>>> 13eb458acaf9e90853bbfca706091fded0cdc631
+
     
 ]
--- food_app/views.py
+++ food_app/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
 
-=======
 def landing_page(request):
-    return render(request,'food_app/index.html')
->>>>>>> 13eb458acaf9e90853bbfca706091fded0cdc631
+    return render(request, 'food_app/index.html')
