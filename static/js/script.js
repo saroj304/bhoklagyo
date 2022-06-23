7d75a5e313f2e073a85a71585460f4d631c0d02b
Ashant Chaudhary <frozen.ashantchy@gmail.com>
2022-06-23 21:42:23 +0545

    Template changes

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+// navbar start
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+hamburger.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",()=>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
+
+// navbar end
