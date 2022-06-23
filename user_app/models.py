b87fdfde66df80b5e620a5d61655f04fc17b1c1d
Ishwor_007 <raayaseetal@gmail.com>
2022-06-23 16:07:30 +0545

    adding user model

--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    email = models.EmailField(max_length=254)
+    phone = models.IntegerField()
+    password = models.CharField(max_length=15)
+    confirm_password = models.CharField(max_length=15)
+    otp = models.CharField(max_length=6)
+    
