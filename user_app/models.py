a5d1ce745b0465c91311bd4fd3ad4571b8f77847
unknown <superstargajanikant@gmail.com>
2022-06-25 13:35:51 +0545

    changed phone integer field to character field

--- user_app/models.py
+++ user_app/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 # Create your models here.
 class User(models.Model):
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=15)
     email = models.EmailField(max_length=254)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=14)
     password = models.CharField(max_length=200)
     #confirm_password = models.CharField(max_length=15)
     #otp = models.CharField(max_length=6)
-    
     
