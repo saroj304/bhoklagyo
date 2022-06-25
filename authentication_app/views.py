6856ee42b6467a1fb6fd84c9a396af56bce378d1
Ishwor_007 <raayaseetal@gmail.com>
2022-06-25 21:39:23 +0545

    sending otp

--- authentication_app/views.py
+++ authentication_app/views.py
@@ -17,20 +17,3 @@ def send_otp_via_email(email):
 
 
 
-
-
-
-
-
-
-
-def sendOtp(phone):
-    conn = http.client.HTTPSConnection("api.msg91.com")
-    authkey = settings.authkey
-    headers ={'content-type':"application/json"}
-    otp = str(random.randint(1000,9999))
-    url = "http://control.msg91.com/api/sendotp.php?otp="+otp+'sender=ABC&message='+'Your otp is '+ otp +'&mobile='+phone+'&authkey='+authkey+'&country=91'
-    conn.request('GET',url,headers=headers)
-    res = conn.getresponse()
-    data = res.read()
-    return None
