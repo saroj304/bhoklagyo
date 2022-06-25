078dd23bcc48f5090df11013ee37a0febb235425
unknown <superstargajanikant@gmail.com>
2022-06-25 13:47:03 +0545

    added authentication url

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,7 +4,7 @@ from . import views as user_views
 
 urlpatterns = [
     path('signup/',user_views.signup_page, name='signup'),
-   path('login/',user_views.login_page, name='login'),
-   path('register/',user_views.register, name='register'),
-   path('get/',user_views.getPassword, name='register'),
+    path('login/',user_views.login_page, name='login'),
+    path('register/',user_views.register, name='register'),
+    path('authenticate/',user_views.login_user, name='register'),
 ]
